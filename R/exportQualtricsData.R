1a6c93e5aea79cdb5299aefd99bb2d54d2cef4e1
Seth Berry <seth.berry@nd.edu>
2016-04-03 16:01:07 -0400

    Revise defaults.

--- R/exportQualtricsData.R
+++ R/exportQualtricsData.R
@@ -1,6 +1,6 @@
 # @export
 exportQualtricsData = function (username = username, token = token, 
-                                format = "XML", surveyID, dropExtra = FALSE) {
+                                format = "CSV", surveyID, dropExtra = FALSE) {
   url = paste("https://survey.qualtrics.com//WRAPI/ControlPanel/api.php?Version=2.5&Request=getLegacyResponseData",
               "&User=", username,
               "&Token=", token,
