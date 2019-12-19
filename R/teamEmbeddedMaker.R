e8802ccadea539995ee934c1bb49162ade62e8e3
Seth Berry <seth.berry@nd.edu>
2019-12-18 22:40:38 -0500

    Creating a function for group member crossings to upload to Qualtrics contacts.

---
+++ R/teamEmbeddedMaker.R
@@ -0,0 +1,81 @@
+#' Creates crossings for each group to upload into Qualtrics.
+#' 
+#' @aliases teamEmbeddedMaker
+#' @author Seth Berry
+#' @description This function takes a vector of student names and creates groups.
+#' @usage teamEmbeddedMaker(teamAssignment)
+#' @param teamAssignmentData A data frame with student names (called "name") and
+#' teams (called "teams").
+#' @details This is most useful for creating embedded data fields for Qualtrics.
+#' @return This will return a data frame with the crossing of students for each
+#' group.
+#' @examples
+#' \dontrun{
+#' 
+#' teamAssignment <- data.frame(name = c("Thomas", "Dirk", "John", "Brian", "Jenny", "Michael", 
+#' "Bill", "Martin", "Douglas", "Hadley"), team = c(rep(1, 3), rep(2, 4), rep(3, 3)))
+#' 
+#' teamEmbeddedMaker(teamAssignment, teamAssignment$team)
+#' 
+#' }
+#' @importFrom dplyr mutate_at
+#' @importFrom dplyr mutate
+#' @importFrom dplyr lead
+#' @importFrom dplyr %>%
+#' @importFrom dplyr n
+#' @importFrom tidyr pivot_wider
+#' @importFrom data.table rbindlist
+#' @export
+
+teamEmbeddedMaker <- function(teamAssignmentData){
+  
+  teamAssignment <- teamAssignmentData
+  
+  allMembers <- lapply(1:teamLength, function(x) {
+    
+    smallSample <- teamAssignment[teamAssignment$team == x, ]  
+    
+    smallSample <- mutate_at(smallSample, "name", as.character) %>% 
+      mutate(personID = 1:n())
+    
+    teamLength <- nrow(smallSample)
+    
+    testVals <- 1:teamLength
+    
+    needs <- vector("list", length = teamLength)
+    
+    i <- 1
+    
+    while(i < (teamLength + 1)) {
+      
+      firstVal <- testVals[1]
+      
+      testVals <- dplyr::lead(testVals)
+      
+      testVals[teamLength] <- firstVal
+      
+      testVals <- testVals
+      
+      needs[[i]] <- as.vector(testVals)
+      
+      i <- i + 1
+    }
+    
+    testDat <- pivot_wider(smallSample, id_cols = personID, values_from = name, 
+                           names_from = personID, names_prefix = "member")
+    
+    testDims <- ncol(testDat)
+    
+    testDat[2:testDims, ] <- ""
+    
+    tmp <- lapply(1:ncol(testDat), function(j) {
+      testDat[1, needs[[j]][1:ncol(testDat)]]
+    })
+    
+    tmp <- data.table::rbindlist(tmp)
+  })
+  
+  allMembers <- data.table::rbindlist(allMembers, fill = TRUE)
+  
+  return(allMembers)
+}
